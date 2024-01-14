1b157a148bcb13b80b6a9cc12fb97d053168d687
FelipeSamsel <felipesamsel@gmail.com>
2024-01-14 10:04:25 -0300

    Aulas 1 a 8 + exercícios

--- aula1.py
+++ aula1.py
@@ -1,1 +1,15 @@
-print ('Hello World!')
+"""
+DocString
+E escrever o que eu
+quiser
+
+
+asdfasdfd
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
---
+++ aula8_Exercicio.py
@@ -0,0 +1,13 @@
+nome = "Felipe"
+sobrenome = "Samsel"
+idade = 30
+ano_de_nascimento = 1993
+maior_de_idade = idade >= 18
+altura_metros = 1.82
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de Nascimento: ", ano_de_nascimento)
+print("É maior de idade? ", maior_de_idade)
+print("Altuma em metros: ", altura_metros)
