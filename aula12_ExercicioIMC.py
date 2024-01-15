9a65f9923ac275f297745a58c45fc48ab9c6ca2a
FelipeSamsel <felipesamsel@gmail.com>
2024-01-15 06:52:14 -0300

    aulas 9 a 14 com exercício de IMC

---
+++ aula12_ExercicioIMC.py
@@ -0,0 +1,12 @@
+nome = "Felipe Z. Samsel"
+peso = 89
+altura = 1.82
+imc = peso / (altura ** 2)
+
+# Felipe tem x de altura
+# pesa x quilos e seu IMC é
+# imc
+
+print(nome + " tem " + str(altura) + " de altura")
+print("pesa " + str(peso) + "kg e seu IMC é:")
+print(imc)
