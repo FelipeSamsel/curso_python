fdeafc04b6316512535aa2adc698ee57a01d8461
Felipe Z. Samsel <122377421+FelipeSamsel@users.noreply.github.com>
2024-05-27 07:55:23 -0300

    Just a start update.
    All the lessons i have in the Python Course that im doing in Udemy.

---
+++ exerciciocontagem.py
@@ -0,0 +1,9 @@
+numero = input("Digite um numero: ")
+base_piramide = int(numero) * 'o'
+qtde_caraceres = len(base_piramide)
+
+print(f"O número digitado foi: {numero} e possui {qtde_caraceres} caracteres")
+
+print(base_piramide)
+
+
